24253bfbbbf8ea1f7b996b080bda05e490f78fc5
cwong239 <cwong239@calpoly.edu>
2025-04-17 19:40:59 -0700

    Added POST functionality

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -36,6 +36,26 @@ function MyApp() {
       });
   }, []);
 
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="container">
       <Table
